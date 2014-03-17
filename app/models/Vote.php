7c1014ed934193f92ab3eff07e8c1fdae7a70faa
Dylan Nguyen <d_nguyen@live.com>
2014-03-16 21:38:38 -0400

    Add comments to Vote model Add created_on field to votes

--- app/models/Vote.php
+++ app/models/Vote.php
@@ -1,6 +1,6 @@
 <?php
 class Vote extends Eloquent {
-    protected $table = 'tracks';
+    protected $table = 'votes';
     public $timestamps = false;
 
     /**
@@ -8,7 +8,7 @@ class Vote extends Eloquent {
      * @param $userid
      * @param $trackid
      *
-     * returns vote
+     * @return stdClass
      */
     public static function get($userid, $trackid) {
         $vote = DB::table('votes')
@@ -24,15 +24,28 @@ class Vote extends Eloquent {
         }
     }
 
+    /**
+     * Inserts a new vote
+     * @param $data
+     *
+     * @return void
+     */
     public static function insert($data) {
         DB::table('votes')
             ->insert(array(
                 'userid' => $data['userid'],
                 'trackid' => $data['trackid'],
-                'liked' => $data['liked']
+                'liked' => $data['liked'],
+                'created_on' => date("Y-m-d H:i:s")
             ));
     }
 
+    /**
+     * Removes a vote by user id and track id
+     * @param $data
+     *
+     * @return void
+     */
     public static function remove($data) {
         DB::table('votes')
             ->where('userid', '=', $data['userid'])
@@ -40,15 +53,29 @@ class Vote extends Eloquent {
             ->delete();
     }
 
+    /**
+     * Updates a vote by user id and track id
+     * @param $data
+     *
+     * @return void
+     */
     public static function updateLiked($data) {
         DB::table('votes')
             ->where('userid', '=', $data['userid'])
             ->where('trackid', '=', $data['trackid'])
             ->update(array(
-                'liked' => $data['liked']
+                'liked' => $data['liked'],
+                'created_on' => date("Y-m-d H:i:s")
             ));
     }
 
+    /**
+     * Checks if a vote exists for a user id and track id
+     * @param $userid
+     * @param $trackid
+     *
+     * @return bool
+     */
     public static function exists($userid, $trackid) {
         $count = DB::table('votes')
             ->select('*')
